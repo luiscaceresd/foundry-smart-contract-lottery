d3b8292bbaee71a39761e0349815b6f3dfc516d8
luiscaceresd <luiscaceresd97@gmail.com>
2024-01-13 19:04:06 -0400

    adding custom errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,26 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
 
 /** 
  * @title A sample Raffle Contract
@@ -9,14 +29,18 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle {
+  error Raffle__NotEnoughEthSent();
+
   uint256 private immutable i_entranceFee;
 
   constructor(uint256 entranceFee) {
     i_entranceFee = entranceFee;
   }
 
-  function enterRaffle() public payable {
-
+  function enterRaffle() external payable {
+    if(msg.value < i_entranceFee ) {
+      revert Raffle__NotEnoughEthSent();
+    }
   }
 
   function pickWinner() public{
@@ -24,8 +48,8 @@ contract Raffle {
   }
 
   /** Getter Functions */
-  
-  function getEntranceFee() external view returns(uint256) {
+
+  function getEntranceFee() external view returns (uint256) {
     return i_entranceFee;
   }
 }
