381f676dcdbdd26d6322181dcc8c96d874feebb4
luiscaceresd <luiscaceresd97@gmail.com>
2024-01-13 18:56:34 -0400

    initializing contract

---
+++ src/Raffle.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+
+/** 
+ * @title A sample Raffle Contract
+ * @author Luis Caceres
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle {
+  uint256 private immutable i_entranceFee;
+
+  constructor(uint256 entranceFee) {
+    i_entranceFee = entranceFee;
+  }
+
+  function enterRaffle() public payable {
+
+  }
+
+  function pickWinner() public{
+
+  }
+
+  /** Getter Functions */
+  
+  function getEntranceFee() external view returns(uint256) {
+    return i_entranceFee;
+  }
+}
