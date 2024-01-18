33064a96699460668194c5b5e5c6419d18d29bbf
Luis Caceres <luiscaceresd97@gmail.com>
2024-01-17 22:02:50 -0400

    adding chainlink coordinator

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -22,6 +22,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import { VRFCoordinatorV2Interface } from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 /** 
  * @title A sample Raffle Contract
  * @author Luis Caceres
@@ -31,19 +33,32 @@ pragma solidity ^0.8.18;
 contract Raffle {
   error Raffle__NotEnoughEthSent();
 
+  /** State Variables */
+  uint16 private constant REQUEST_CONFIRMATIONS = 3;
+  uint32 private constant NUM_WORDS = 1;
+
   uint256 private immutable i_entranceFee;
-  // @dev Duration of the lottery in seconds
   uint256 private immutable i_interval;
+  VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+  bytes32 private immutable i_gasLane;
+  uint64 private immutable i_subscriptionId;
+  uint32 private immutable i_callbackGasLimit;
+
   address payable[] private s_players;
   uint256 private s_lastTimeStamp;
 
   /** Events */
   event EnteredRaffle(address indexed player);
 
-  constructor(uint256 entranceFee, uint256 interval) {
+  constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) {
     i_entranceFee = entranceFee;
     i_interval = interval;
+    i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+    i_gasLane = gasLane;
+    i_subscriptionId = subscriptionId;
+    i_callbackGasLimit = callbackGasLimit;
     s_lastTimeStamp = block.timestamp;
+    
   }
 
   function enterRaffle() external payable {
@@ -56,7 +71,18 @@ contract Raffle {
   }
 
   function pickWinner() external{
-    block.timestamp - s_lastTimeStamp > i_interval;
+    //check if enough time has passed
+    if(block.timestamp - s_lastTimeStamp < i_interval){
+      revert();
+    }
+
+    uint256 requestId = i_vrfCoordinator.requestRandomWords(
+      i_gasLane, //gas limit
+      i_subscriptionId,
+      REQUEST_CONFIRMATIONS,
+      i_callbackGasLimit,
+      NUM_WORDS
+    );
   }
 
   /** Getter Functions */
