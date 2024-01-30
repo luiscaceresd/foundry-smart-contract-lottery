26a063c70a5311afc2571f8c76ad84f03b69e8d0
luiscaceresd <luiscaceresd97@gmail.com>
2024-01-29 22:44:27 -0400

    adding event tests

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -7,6 +7,9 @@ import {Test, console} from "forge-std/Test.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
 contract RaffleTest is Test {
+  /* Events */
+  event EnteredRaffle(address indexed player);
+
   Raffle raffle;
   HelperConfig helperConfig;
   uint256 entranceFee;
@@ -51,5 +54,12 @@ contract RaffleTest is Test {
       raffle.enterRaffle{value: entranceFee}();
       address playerRecorded = raffle.getPlayer(0);
       assert(playerRecorded == PLAYER);
+    }
+
+    function testEmitsEventOnEntrance() public {
+      vm.prank(PLAYER);
+      vm.expectEmit(true, false, false, false, address(raffle));
+      emit EnteredRaffle(PLAYER);
+      raffle.enterRaffle{value: entranceFee}();
     }
 }
