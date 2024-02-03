1b4e65c65dd2c08ba3e137a1fec255b0f09efc6c
luiscaceresd <luiscaceresd97@gmail.com>
2024-02-03 13:05:40 -0400

    adding add consumer script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,6 +5,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {VRFCoordinatorV2Mock} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 import {LinkToken} from "../test/mocks/LinkToken.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract CreateSubscription is Script {
     function createSubscriptionUsingConfig() public returns (uint64) {
@@ -57,25 +58,52 @@ contract FundSubscription is Script {
         console.log("Funding subscription: ", subId);
         console.log("Using vrfCoordinator: ", vrfCoordinator);
         console.log("On ChainID: ", block.chainid);
-        if(block.chainid == 31337) {
-          vm.startBroadcast();
-          VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(
-              subId,
-              uint96(FUND_AMOUNT)
-          );
-          vm.stopBroadcast();
+        if (block.chainid == 31337) {
+            vm.startBroadcast();
+            VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(
+                subId,
+                uint96(FUND_AMOUNT)
+            );
+            vm.stopBroadcast();
         } else {
-          vm.startBroadcast();
-          LinkToken(link).transferAndCall(
-              vrfCoordinator,
-              FUND_AMOUNT,
-              abi.encode(subId)
-          );
-          vm.stopBroadcast();
+            vm.startBroadcast();
+            LinkToken(link).transferAndCall(
+                vrfCoordinator,
+                FUND_AMOUNT,
+                abi.encode(subId)
+            );
+            vm.stopBroadcast();
         }
     }
 
     function run() external {
         fundSubscriptionUsingConfig();
+    }
+}
+
+contract AddConsumer is Script {
+
+    function addConsumer (address raffle, address vrfCoordinator, uint64 subId) public {
+        console.log("Adding consumer contract ", raffle);
+        console.log("Using vrfCoordinator: ", vrfCoordinator);
+        console.log("On ChainID: ", block.chainid);
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock(vrfCoordinator).addConsumer(subId, raffle);
+        vm.stopBroadcast();
+    }
+
+    function addConsumerUsingConfig(address raffle) public {
+        HelperConfig helperConfig = new HelperConfig();
+        (, , address vrfCoordinator, , uint64 subId, , ) = helperConfig
+            .activeNetworkConfig();
+        addConsumer(raffle, vrfCoordinator, subId);
+    }
+
+    function run() external {
+        address raffle = DevOpsTools.get_most_recent_deployment(
+            "Raffle",
+            block.chainid
+        );
+        addConsumerUsingConfig(raffle);
     }
 }
