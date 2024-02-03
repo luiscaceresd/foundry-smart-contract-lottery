eee6343c3feb7a525ca33ded38a54a82e9aadcce
luiscaceresd <luiscaceresd97@gmail.com>
2024-02-02 22:45:40 -0400

    updating interactions

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.18;
 import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {VRFCoordinatorV2Mock} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {LinkToken} from "../test/mocks/LinkToken.sol";
 
 contract CreateSubscription is Script {
     function createSubscriptionUsingConfig() public returns (uint64) {
         HelperConfig helperConfig = new HelperConfig();
-        (, , address vrfCoordinator, , , , ) = helperConfig.activeNetworkConfig();
+        (, , address vrfCoordinator, , , , ) = helperConfig
+            .activeNetworkConfig();
         return createSubscription(vrfCoordinator);
     }
 
@@ -35,7 +37,42 @@ contract FundSubscription is Script {
 
     function fundSubscriptionUsingConfig() public {
         HelperConfig helperConfig = new HelperConfig();
-        (, , address vrfCoordinator, , , uint64 subId, address link) = helperConfig.activeNetworkConfig();
+        (
+            ,
+            ,
+            address vrfCoordinator,
+            ,
+            ,
+            uint64 subId,
+            address link
+        ) = helperConfig.activeNetworkConfig();
+        fundSubscription(vrfCoordinator, subId, link);
+    }
+
+    function fundSubscription(
+        address vrfCoordinator,
+        uint64 subId,
+        address link
+    ) public {
+        console.log("Funding subscription: ", subId);
+        console.log("Using vrfCoordinator: ", vrfCoordinator);
+        console.log("On ChainID: ", block.chainid);
+        if(block.chainid == 31337) {
+          vm.startBroadcast();
+          VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(
+              subId,
+              uint96(FUND_AMOUNT)
+          );
+          vm.stopBroadcast();
+        } else {
+          vm.startBroadcast();
+          LinkToken(link).transferAndCall(
+              vrfCoordinator,
+              FUND_AMOUNT,
+              abi.encode(subId)
+          );
+          vm.stopBroadcast();
+        }
     }
 
     function run() external {
